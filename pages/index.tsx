45d26ea55c60c5d867b721b4541fbe6207978698
MateusArenas <MateusArenas97@gmail.com>
2022-07-18 16:22:01 -0300

    add main and section one and footer new link remove href n

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,8 +22,8 @@ const Home: NextPage = () => {
             <div className="row align-items-center px-md-5">
               <div className="col-12 col-md-6">
                 <div className="row justify-content-start">
-                  <div className="card bg-dark text-white">
-                    <div className="card-header">
+                  <div className="card border-0 bg-transparent text-white">
+                    <div className="card-header bg-transparent border-0">
                       Mateus Arenas
                     </div>
                     <div className="card-body">
