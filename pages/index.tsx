4991f1b463e6c0c33b4e71f5c552bbf6405e836b
MateusArenas <MateusArenas97@gmail.com>
2022-07-18 14:31:38 -0300

    add main and section one and footer new link remove href n

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,9 @@ const Home: NextPage = () => {
                   <div className="card-body">
                     <h1 className="card-title">The Simple, Clean Designer</h1>
                     <p className="card-text">Agency provides a full service range including technical skills, design, business understanding.</p>
-                    <a className="btn btn-primary">See My Work</a>
+                    <Link href={"#"}>
+                      <a className="btn btn-primary">See My Work</a>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -55,7 +57,7 @@ const Home: NextPage = () => {
                   <div className="card-body">
                     <h3 className="card-title">Design</h3>
                     <p className="card-text">Agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house.</p>
-                    <a className="btn btn-primary">Learn More</a>
+                    <Link href={"#"}><a className="btn btn-primary">Learn More</a></Link>
                   </div>
                 </div>
               </div>
@@ -65,7 +67,7 @@ const Home: NextPage = () => {
                   <div className="card-body">
                     <h3 className="card-title">Design</h3>
                     <p className="card-text">Agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house.</p>
-                    <a className="btn btn-primary">Learn More</a>
+                    <Link href={"#"}><a className="btn btn-primary">Learn More</a></Link>
                   </div>
                 </div>
               </div>
@@ -83,7 +85,7 @@ const Home: NextPage = () => {
         <div className="container py-4 pt-md-5 px-4 px-md-3">
           <div className="row">
             <div className="col-lg-3 mb-3">
-              <a className="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href="/" aria-label="Bootstrap">
+              <a className="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" aria-label="Bootstrap">
                 {/* <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" className="d-block me-2" viewBox="0 0 118 94" role="img"><title>Bootstrap</title><path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" fill="currentColor"></path></svg> */}
                 <span className="fs-5">Personal</span>
               </a>
