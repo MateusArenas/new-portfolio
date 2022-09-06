5519aa1f7c62604fdf3057fe6bde53ea855c90b6
MateusArenas <MateusArenas97@gmail.com>
2022-09-06 14:29:43 -0300

    add all projects and add image in header hero

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+import ip from "ip";
+
+console.dir ( ip.address() );
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', 
+  baseURL: `http://${ip.address()}:3000/api`, 
   headers: {
     'Access-Control-Allow-Origin': '*',
     Connection: 'keep-alive',
