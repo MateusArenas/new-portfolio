6f98e9b3ae31cbb7d36b2e2dd76827724a6350ae
MateusArenas <MateusArenas97@gmail.com>
2022-09-16 22:50:22 -0300

    last-modify 2

--- content.tsx
+++ content.tsx
@@ -2,10 +2,18 @@ import { DiPhp, DiJavascript1, DiPython } from 'react-icons/di';
 
 export const content = {
   "title": "Mateus Arenas Gioio",
-  "description": "Sou um Desenvolvedor de Sistemas Web e Mobile, tendo como foco principal a linguagem Javascript/ Typescript e o Freamework React Native. Faço construções de APIs utilizando Node Js e MongoDB. Atualmente trabalho com Freelancer e estou em fase de conclusão de estudos em Analise e Desenvolvedor de Sistemas.",
   "username": "MateusArenas",
   "email": "MateusArenas97@gmail.com",
   "whatsapp": "5511949123337",
+  "github": "https://github.com/MateusArenas",
+  "linkedin": "https://www.linkedin.com/in/mateus-arenas-11965518a/",
+  "instagram": "https://www.instagram.com/arenas_mat/",
+  "copyright": "Copyright © 2022 Mateus Arenas Gioio. All Rights Reserved.",
+  "about": { 
+    "title": "Um pouco sobre min",
+    "description": "Sou um Desenvolvedor de Sistemas Web e Mobile, tendo como foco principal a linguagem Javascript/ Typescript e o Freamework React Native. Faço construções de APIs utilizando Node Js e MongoDB. Atualmente trabalho com Freelancer e estou em fase de conclusão de estudos em Analise e Desenvolvedor de Sistemas.",
+    "calltoactions": { "projects": "Projetos", "qualifications": "Qualificações", "contact": "Entrar em contato" },
+   },
   "qualifications": {
     "title": "Qualificações",
     "info": "Habilidades",
--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ const Home: NextPage<{ data?: IProject[], pinneds: any[] }> = ({ pinneds }) => {
         <meta property="og:image:height" content="460" /> 
 
         <title>{content.title}</title>
-        <meta name="description" content={content.description} />
+        <meta name="description" content={content.about.description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -74,17 +74,17 @@ const Home: NextPage<{ data?: IProject[], pinneds: any[] }> = ({ pinneds }) => {
                   <div className="card border-0 bg-transparent text-white">
                     <Link passHref href={"#contact"}>
                       <a className="card-header bg-transparent border-0 text-decoration-none app-text-primary">
-                        Entrar em contato
+                        {content.about.calltoactions.contact}
                       </a>
                     </Link>
                     <div className="card-body">
-                      <h1 className="card-title">Um pouco sobre min</h1>
-                      <p className="card-text">{content.description}</p>
+                      <h1 className="card-title">{content.about.title}</h1>
+                      <p className="card-text">{content.about.description}</p>
                       <Link href={"#projects"}>
-                        <a className="btn btn-primary app-bg-primary border-0 me-2">Projetos</a>
+                        <a className="btn btn-primary app-bg-primary border-0 me-2">{content.about.calltoactions.projects}</a>
                       </Link>
                       <Link href={"#courses"}>
-                        <a className="btn btn-outline-secondary">Qualificações</a>
+                        <a className="btn btn-outline-secondary">{content.about.calltoactions.qualifications}</a>
                       </Link>
                     </div>
                   </div>
@@ -342,7 +342,7 @@ const Home: NextPage<{ data?: IProject[], pinneds: any[] }> = ({ pinneds }) => {
             <div className="col-lg-3 mb-3">
               <a className="d-inline-flex align-items-center mb-2 text-white text-decoration-none" aria-label="Bootstrap">
                 {/* <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" className="d-block me-2" viewBox="0 0 118 94" role="img"><title>Bootstrap</title><path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" fill="currentColor"></path></svg> */}
-                <span className="fs-5">Mateus Arenas Gioio</span>
+                <span className="fs-5">{content.title}</span>
               </a>
             </div>
             <div className="col-6 col-lg-3 offset-lg-1 mb-3">
@@ -355,31 +355,31 @@ const Home: NextPage<{ data?: IProject[], pinneds: any[] }> = ({ pinneds }) => {
               </ul>
             </div>
             <div className="col-6 col-lg-3 mb-3">
-              <h5 className="mb-4">Qualificações</h5>
+              <h5 className="mb-4">{content.qualifications.title}</h5>
               <ul className="list-unstyled">
-                <li className="mb-2"><Link passHref href="#courses"><a className="text-decoration-none text-white">Javascript</a></Link></li>
-                <li className="mb-2"><Link passHref href="#courses"><a className="text-decoration-none text-white">PHP</a></Link></li>
-                <li className="mb-2"><Link passHref href="#courses"><a className="text-decoration-none text-white">Python</a></Link></li>
+                {content.qualifications.items.map(item => (
+                  <li key={item.title} className="mb-2"><Link passHref href="#courses"><a className="text-decoration-none text-white">{item.title}</a></Link></li>
+                ))}
               </ul>
             </div>
             <div className="col-6 col-lg-2 mb-3">
               <ul className="list-unstyled row">
                 <li className="mb-2 pe-2 col-4">
-                  <Link passHref href="https://www.instagram.com/arenas_mat/">
+                  <Link passHref href={content.instagram}>
                     <a className='text-white'>
                       <FaInstagram size={24*1.8} />
                     </a>
                   </Link>
                 </li>
                 <li className="mb-2 pe-2 col-4">
-                  <Link passHref href="https://www.linkedin.com/in/mateus-arenas-11965518a/">
+                  <Link passHref href={content.linkedin}>
                     <a className='text-white'>
                       <FaLinkedin size={24*1.8} />
                     </a>
                   </Link>
                 </li>
                 <li className="mb-2 pe-2 col-4">
-                  <Link passHref href="https://github.com/MateusArenas">
+                  <Link passHref href={content.github}>
                     <a className='text-white'>
                       <FaGithub size={24*1.8} />
                     </a>
@@ -392,7 +392,7 @@ const Home: NextPage<{ data?: IProject[], pinneds: any[] }> = ({ pinneds }) => {
           <div className="row mt-5 mb-5 mb-lg-0">
             
             <div className="col-12 col-md-6">
-              <p>Copyright © 2022 Mateus Arenas Gioio. All Rights Reserved.</p>
+              <p>{content.copyright}</p>
             </div>
 
             {/* <div className="col-12 col-md-6 text-center text-md-end">
